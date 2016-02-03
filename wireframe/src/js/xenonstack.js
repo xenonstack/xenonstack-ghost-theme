f830ecfe2118e1d9dcb49152a9ffbc58d9c3178c
BeliefLimitless <belieflimitless@gmail.com>
2016-02-03 17:17:56 +0530

    Deleted files.

---
+++ wireframe/src/js/xenonstack.js
@@ -0,0 +1,17 @@
+/*
+
+XenonStack JS
+Authors: Ankita Dhiman, Abhay Arora.
+Copyright (c) 2016, XenonStack LLC.
+
+ */
+
+(function(_W){
+
+    _D = _W.document;
+    _B = _D.body;
+
+    $(document).ready(function () {
+    });
+
+})(window);
